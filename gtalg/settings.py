747221c20593d32f048948f9fb628aaaa9990e5b
Luca Leonardo Bologna <lucaleonardo.bologna@cnr.it>
2020-02-21 18:17:09 +0100

    fixed indentation problems

--- gtalg/settings.py
+++ gtalg/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from gtalg-keys import keys
+from gtalg_keys import keys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
